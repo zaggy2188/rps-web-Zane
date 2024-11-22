c52d10bc203e60a9f4f3ce38e047959468a4bb0b
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2024-11-22 14:51:45 +0000

    set rounds

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+function setRounds(){
+    let rounds = prompt("How many rounds?");
+    if (rounds % 2 === 0) {
+        alert("must be odd");
+        return setRounds();
+    }
+    else return rounds;
+    //working//
+}
