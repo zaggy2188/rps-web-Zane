4f4b82a1fbc0f75e4858f1d0f53fa667c3210baf
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2024-11-22 17:17:49 +0000

    eBlock

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-function setRounds(){
-    let rounds = prompt("How many rounds?");
-    if (rounds % 2 === 0) {
+
+function getRounds(){
+    let rounds = document.getElementById("rounds").value;
+    setRounds(rounds);
+}
+
+function setRounds(rounds){
+    if (rounds % 2 == 0) {
         alert("must be odd");
-        return setRounds();
     }
-    else return rounds;
-    //working//
+    localStorage.setItem("rounds",rounds);
+    window.location.replace("chooser.html");
+}
+
+
+function cpuTurn(u){
+    let moves = ["r","p","s"];
+    let choice = Math.floor(Math.random()*3);
+    let c = moves[choice];
+    findWinner(u,c);
+}
+
+function findWinner(u,c){
+    let winner = " ";
+    let winArray=[["r","p","I"],["r","s","you"],["p","s","I"],["p","r","you"],["s","r","I"],["s","p","you"]];
+        for (let i = 0; i< winArray.length; i++){
+            if (winArray[i][0] == u && winArray[i][1]==c){
+                winner= winArray[i][2];
+
+            }
+        }
+    alert("You choose " + u + " and I choose " + c + " " + winner + " win!");
+    return winner;
 }
